88b9390438d69d9a3674b75b87784b9aaf5c8c69
Chris West <chris@minrivertea.com>
2011-06-16 09:44:53 +0800

    changes

--- registration/views.py
+++ registration/views.py
@@ -214,7 +214,7 @@ def register(request, backend, success_url=None, form_class=RegistrationFormNoUs
                           fail_silently=False,
             )
 
-            success_url = "/laowai/%s" % new_laowai.id
+            success_url = "/feed/laowai/%s" % new_laowai.id
 
 
 
