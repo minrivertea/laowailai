67124a4c364ccb6d4dadea3e2c21b4599a1f0cac
Chris West <chris@minrivertea.com>
2011-06-16 21:14:34 +0800

    nothing

--- list/views.py
+++ list/views.py
@@ -45,12 +45,8 @@ def index(request):
 # news feed for a particular city
 def news_feed(request, slug):
     city = get_object_or_404(City, slug=slug) 
-    infos = Info.objects.all()
-    for i in infos:
-        i.city = i.added_by.city
-        i.save()
             
-    # infos = Info.objects.filter(city=city).order_by('-date_added')
+    infos = Info.objects.filter(city=city).order_by('-date_added')
     objects = []
     for i in infos:
         if request.user.is_authenticated():
