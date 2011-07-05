446d79c05614717e3983773de4ba9751cf68ee49
Chris West <chris@minrivertea.com>
2011-07-05 12:13:57 +0800

    migrations step one

--- list/admin.py
+++ list/admin.py
@@ -1,9 +1,11 @@
 from laowailai.list.models import *
 from django.contrib import admin
 
+class NewInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'city')
 
 admin.site.register(Likes)
-admin.site.register(NewInfo)
+admin.site.register(NewInfo, NewInfoAdmin)
 #admin.site.register(CommonInfo)
 admin.site.register(Laowai)
 admin.site.register(Subscriber)
