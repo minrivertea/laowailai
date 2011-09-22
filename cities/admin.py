3dd60e9dcd357948e80a2fae4d31ead364f700bf
Chris <chris@fry-it.com>
2011-09-22 08:53:56 +0800

    changes

--- cities/admin.py
+++ cities/admin.py
@@ -1,9 +1,11 @@
 from laowailai.cities.models import *
 from django.contrib import admin
 
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('url', 'city', 'approved')
 
 admin.site.register(City)
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
 
 
 
